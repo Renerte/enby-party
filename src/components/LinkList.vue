c9007035a4e5218557eaaa6ceef32cc7066f079c
Damian Suchy <contact@renerte.net>
2021-02-27 18:33:51 +0100

    Implement link list in Vue

---
+++ src/components/LinkList.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div.list
+  a.list-item(v-for='link in links' v-bind:href="link.url" target="_blank") {{link.label}}
+</template>
+
+<script lang="ts">
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      links: []
+    }
+  },
+  async mounted() {
+    let linksReq = await axios.get('https://cdn.renerte.net/enby/links.json');
+    this.links = linksReq.data;
+  }
+}
+</script>
---
+++ src/script.ts
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import LinkList from './components/LinkList.vue';
+
+new Vue(LinkList).$mount('#link-list');
