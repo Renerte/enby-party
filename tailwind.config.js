691e26cb742a99a329022c4f912aa848a3a735f5
Renn Suchy <contact@renerte.net>
2021-05-21 15:11:09 +0200

    Add Tailwind & PostCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // mode: 'jit', // TODO: Tailwind JIT does not work with sass
+    purge: ['./src/**/*.html', './src/**/*.ts', './src/**/*.vue'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
